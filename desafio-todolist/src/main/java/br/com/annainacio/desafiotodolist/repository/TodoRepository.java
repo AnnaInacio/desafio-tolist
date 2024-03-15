3bc68a76df220c32612e45ef5ebd1d82786eaf66
Anna Inácio <55771360+AnnaInacio@users.noreply.github.com>
2024-03-14 23:44:19 -0300

    Delete desafio-todolist directory

--- desafio-todolist/src/main/java/br/com/annainacio/desafiotodolist/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.annainacio.desafiotodolist.repository;
-
-import br.com.annainacio.desafiotodolist.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
